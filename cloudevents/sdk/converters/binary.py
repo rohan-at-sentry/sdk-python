88100c1a8b704e40859552cfdaeb29f89f6670de
Rohan Bhaumik <rohanbhaumik@FVFHQ2MKQ05P.local>
2023-08-22 10:55:45 -0400

    adding a small comment to trigger CI run

--- cloudevents/sdk/converters/binary.py
+++ cloudevents/sdk/converters/binary.py
@@ -11,7 +11,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-#  This is a test, pl ignore
+#  This is a test, pl ignore my name is Rohan I work at Sentry.io
 
 import typing
 
